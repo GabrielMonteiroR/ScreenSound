8496f95fb7fcd019ce04ad898d3010b912aca6b3
Gabriel <gmonteiro810@gmail.com>
2024-09-28 10:00:21 -0300

    instanciando objetos

---
+++ ScreenSound/Music.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound
+{
+    internal class Music
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public double Duration { get; set; }
+        public bool IsAvaliable { get; set; }
+    }
+}
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ScreenSound;
+
+Music musicOne = new Music();
+Music musicTwo = new Music();
+
+
+musicOne.Name = "Musica 1";
+musicOne.Author = "Autor 1";    
+musicOne.Duration = 3.5;        
+musicOne.IsAvaliable = true;   
+
+
+musicTwo.Name = "Musica 2";
+musicTwo.Author = "Autor 2";    
+musicTwo.Duration = 4.2;       
+musicTwo.IsAvaliable = true;
